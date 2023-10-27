067dbd9e9c2bcc8d837b40083c12ddddc21d5783
sourav.kumar.all.2021@gmail.com <sourav.kumar.all.2021@gmail.com>
2023-10-27 22:32:42 +0530

    revamped ux

--- script.js
+++ script.js
@@ -27,16 +27,16 @@ class Menu {
   }
 
   async init() {
-    const data = await fetch('/paths.json').then(e => e.json())
+    const data = await fetch('paths.json').then(e => e.json())
     // const prev = document.getElementById('prev-btn')
 
     for (let id in data) {
-
-      this.addItem(data[id].name, data[id].path)
+      if(data[id].type == 'file') this.addItem(data[id].name, data[id].path)
+      if(data[id].type == 'folder') this.addFolder(data[id].name, data[id].path)
     }
   }
 
-  addFolder(data) {
+  addFolder(name, src) {
     const root = this.root.appendChild(document.createElement('div'))
     root.style.cssText = `
       border-top: 1px solid darkgray;
