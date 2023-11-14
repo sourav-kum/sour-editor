071d782c78fb68f2a7e1fb6db37076ca990fff3c
sourav.kumar.all.2021@gmail.com <sourav.kumar.all.2021@gmail.com>
2023-11-14 20:26:16 +0530

    added doc for sour lang

--- js/highlighter.js
+++ js/highlighter.js
@@ -1,8 +1,28 @@
-document.onload = () => {
-  document.querySelectorAll('language-sourlang').forEach(highlight)
+String.prototype.highlight = function(name) {
+  return `<span class="${name}">${this}</span>`
 }
 
+document.querySelectorAll('.language-sourlang').forEach(highlight)
+
 function highlight(element) {
+  var funs = []
+  
   element.innerHTML = element.innerText
-    .replaceAll(/fun/, '<span class="keyword">$1</span>')
+    .replaceAll(/"(.*)"/g, '<span class="string">"$1"</span>')
+    .replaceAll(/(\d+)/g, '<span class="number">$1</span>')
+    
+    .replaceAll(/(\w+)\(/g, (match, name) => {
+      var index = funs.push(name) - 1
+      return `~${index}(`
+    })
+    
+    .replaceAll(/(void|int|return)/g, '<span class="keyword">$1</span>')
+    .replaceAll(/(fun|var)/g, '<span class="defination">$1</span>')
+    .replaceAll(/(\/\/.+)/g, '<span class="comment">$1</span>')
+    .replaceAll(/(true|false)/g, '<span class="boolean">$1</span>')
+    .replaceAll(/(\/\*.*\*\/)/gs, '<span class="comment">$1</span>')
+    
+    .replaceAll(/#\((.*),(.*)\)/g, `<span class="error">$1</span>`)
+    
+    .replaceAll(/~(\d+)/g, (match, index) => `${funs[index]}`.highlight('function'))
 }
